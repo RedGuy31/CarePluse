bd98550fff31f5d4dacd645dfc588a6241977ff5
Irakli Maisuradze <irakli.maisuradze31.19@gmail.com>
2024-07-22 18:48:55 +0400

    create reuseble form and validations added form features

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,5 +1,86 @@
+"use client";
+import { z } from "zod";
+import { Form } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import { UserFormValidation } from "@/lib/validations";
+import "react-phone-number-input/style.css";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+
 function PatientForm() {
-  return <div>Form</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = async ({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+    try {
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className=" flex-1 space-y-6"
+      >
+        <section className=" mb-12 space-y-4">
+          <h1 className="header">Hi there</h1>
+          <p className=" text-dark-700">Get started with appointmets.</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(+995) 555 534 123"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 }
 
 export default PatientForm;
